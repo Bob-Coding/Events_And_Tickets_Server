2b82710f120759ff964e2638a9c857d96a6402c1
Bob-Coding <bobgunsing@gmail.com>
2020-05-08 22:27:04 +0200

    config file heroku

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
 const url =
   process.env.DATABASE_URL ||
-  "postgresql://postgres:secret@localhost:5432/postgres";
+  
 const db = new Sequelize(url);
 
 db.sync({ force: false })
